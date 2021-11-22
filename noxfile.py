599d1a3aaacbff4a1ae959d72a33f0cf85e5d127
Nejc Skofic <nejc.skofic@gmail.com>
2021-11-22 21:56:42 +0100

    Added linters and pre-commit hooks

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,12 @@ nox.options.default_venv_backend = "conda"
 
 
 @nox_poetry.session(python=["3.9", "3.8", "3.7"])
-def tests(session: nox_poetry.Session):
+def tests(session: nox_poetry.Session) -> None:
     session.install("pytest", "pytest-cov", ".")
     session.run("pytest", "--cov=sqlgraphql", "tests/")
+
+
+@nox_poetry.session(python="3.9")
+def lint(session: nox_poetry.Session) -> None:
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all")
